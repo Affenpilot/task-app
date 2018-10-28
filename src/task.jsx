e83579a410f78c4086e20e1426a14478565c0a37
Florian Buechner <flori.buechner90@googlemail.com>
2018-10-28 17:17:44 +0100

    some changes in draggable

--- src/task.jsx
+++ src/task.jsx
@@ -7,19 +7,30 @@ const Container = styled.div`
     border: 1px solid lightgray;
     border-radius: 4px;
     padding: 8px;
-    background-color: white;
+    background-color: ${props => (props.isDragging ? 'Lightgreen': 'white')};
+    
+    display: flex;
+`;
+
+const Handle = styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: orange;
+    border-radius: 4px;
+    margin-right: 8px;
 `;
 
 export default class Task extends React.Component {
     render() {
         return (
             <Draggable draggableId={this.props.task.id} index={this.props.index}>
-                {(provided) => (
+                {(provided, snapshot) => (
                     <Container
                         {...provided.draggableProps}
-                        {...provided.dragHandleProps}
                         ref={provided.innerRef}
+                        isDragging={snapshot.isDragging}
                     >
+                        <Handle {...provided.dragHandleProps} />
                         {this.props.task.content}
                     </Container>
                 )}
